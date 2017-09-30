41ba134d4a8c37b2a923e9e0dbcfbaeeb2d73f01
Michael Brian Puckett, II <michael_puckett_ii@live.com>
2017-09-30 14:33:04 -0400

    Added tower base

---
+++ VirusTowerDefense/Models/Towers/Tower.cs
@@ -0,0 +1,9 @@
+﻿namespace VirusTowerDefense.Models.Towers
+{
+    public class Tower
+    {
+        public double Health { get; set; }
+        public double Damage { get; set; }
+        public double Range { get; set; }
+    }
+}
