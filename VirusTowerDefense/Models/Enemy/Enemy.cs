fd10cbc3897a0a144df11b4fdbe11be4530239b4
Michael Puckett III <michaelpuckettt@gmail.com>
2017-09-30 14:06:06 -0400

    Added enemy base model

---
+++ VirusTowerDefense/Models/Enemy/Enemy.cs
@@ -0,0 +1,10 @@
+﻿namespace VirusTowerDefense.Models.Enemy
+{
+    public class Enemy
+    {
+        public double Health { get; set; }
+        public double Speed { get; set; }        
+        public double Damage { get; set; }
+        //TODO: Buffs
+    }
+}
