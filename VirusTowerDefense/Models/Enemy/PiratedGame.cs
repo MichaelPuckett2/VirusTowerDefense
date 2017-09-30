12de7644e856a7ba74bdea7d27405614486de799
Michael Puckett III <michaelpuckettt@gmail.com>
2017-09-30 14:49:00 -0400

    removed damage

---
+++ VirusTowerDefense/Models/Enemy/PiratedGame.cs
@@ -0,0 +1,11 @@
+﻿namespace VirusTowerDefense.Models.Enemy
+{
+    public class PiratedGame: Enemy
+    {
+        public PiratedGame()
+        {
+            Health = 20;
+            Speed = .5;
+        }
+    }
+}
