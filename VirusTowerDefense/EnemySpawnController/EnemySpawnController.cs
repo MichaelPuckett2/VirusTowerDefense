14ae613bf020e82f1c977289f0235e40c6126d88
Michael Brian Puckett, II <michael_puckett_ii@live.com>
2017-09-30 16:46:43 -0400

    added basic spawn controller.  Only looks up to wave 3 for now.

---
+++ VirusTowerDefense/EnemySpawnController/EnemySpawnController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using VirusTowerDefense.Models.Enemy;
+
+namespace VirusTowerDefense.EnemySpawnController
+{
+    public class EnemySpawnController
+    {
+        private const int DefaultNumberOfEnemies = 10;
+        private const int EnemyVSRoundMultiplier = 2;
+
+        public IList<Enemy> SpawnEnemies(int waveNumber)
+        {
+            var numberOfEnemies = DefaultNumberOfEnemies + waveNumber * EnemyVSRoundMultiplier;
+            var enemies = new List<Enemy>();
+            var random = new Random();
+            Enemy newEnemy = null;
+
+            for (int i = 0; i < numberOfEnemies; i++)
+            {
+                var randomValue = random.Next(0, waveNumber);
+
+                switch (waveNumber)
+                {
+                    case 0:
+                    case 1:
+                    case 2:
+                        newEnemy = new Malware();
+                        break;
+
+                    case 3:
+                        if (randomValue > 1)
+                            newEnemy = new Spyware();
+                        else
+                            newEnemy = new Malware();
+                        break;
+
+                    default:
+                        if (waveNumber > 3)
+                        {
+                            if (randomValue > 1)
+                                newEnemy = new Spyware();
+                            else
+                                newEnemy = new Malware();
+                            break;
+                        }
+                        break;
+                }
+
+                if (newEnemy != null)
+                    enemies.Add(newEnemy);
+            }
+
+            return enemies;
+        }
+    }
+}
