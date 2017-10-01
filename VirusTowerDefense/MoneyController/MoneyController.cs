1db509e8acaa010ab401accfc65570e74fb83e1a
Michael Puckett III <michaelpuckettt@gmail.com>
2017-09-30 21:25:52 -0400

    added MoneyController

---
+++ VirusTowerDefense/MoneyController/MoneyController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirusTowerDefense.Models.Enemy;
+
+namespace VirusTowerDefense.ScoreController
+{
+    public class MoneyController
+    {
+        public int Score { get; set; }
+
+        public void VirusEnteredKernal(Enemy enemy)
+        {
+            var value = 0;
+
+            switch (enemy)
+            {
+                case BotAttack botAttack:
+                    value = 10;
+                    break;
+
+                case Malware malware:
+                    value = 10;
+                    break;
+
+                case PiratedGame piratedGame:
+                    value = 10;
+                    break;
+
+                case Spyware spyware:
+                    value = 10;
+                    break;
+
+                default:
+                    break;
+            }
+
+            Score -= value;
+        }
+
+        public void VirusDefeated(Enemy enemy)
+        {
+            var value = 0;
+
+            switch (enemy)
+            {
+                case BotAttack botAttack:
+                    value = 5;
+                    break;
+
+                case Malware malware:
+                    value = 10;
+                    break;
+
+                case PiratedGame piratedGame:
+                    value = 35;
+                    break;
+
+                case Spyware spyware:
+                    value = 20;
+                    break;
+
+                default:
+                    break;
+            }
+
+            Score += value;
+        }
+
+        public void WaveComplete(int waveNumber)
+        {
+            Score += 500;
+        }
+    }
+}
